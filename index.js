836dd16139afc10e1f9bb4d035df6dcc642575b4
eduardofoliveira <eduardo_felipe_oliveira@yahoo.com.br>
2019-04-28 16:55:25 -0300

    Desenvolvendo

--- index.js
+++ index.js
@@ -23,14 +23,7 @@ const contemNaLista = id => {
 conn = new esl.Connection("127.0.0.1", 8021, "ClueCon", function() {
   conn.events("json", "all");
 
-  conn.subscribe([
-    "CHANNEL_CREATE",
-    "CHANNEL_CALLSTATE",
-    "CHANNEL_STATE",
-    "CHANNEL_EXECUTE",
-    "CHANNEL_EXECUTE_COMPLETE",
-    "CHANNEL_DESTROY"
-  ]);
+  conn.subscribe(["CHANNEL_CREATE", "CHANNEL_HANGUP_COMPLETE"]);
 
   conn.on("esl::event::CHANNEL_CREATE::*", evento => {
     if (evento.getHeader("Call-Direction") === "outbound") {
@@ -51,15 +44,13 @@ conn = new esl.Connection("127.0.0.1", 8021, "ClueCon", function() {
   });
 
   conn.on("esl::event::CHANNEL_HANGUP_COMPLETE::*", evento => {
-    if (evento.getHeader("Call-Direction") === "outbound") {
-      const chamada = {
-        callid: evento.getHeader("Channel-Call-UUID"),
-        from: evento.getHeader("Caller-Caller-ID-Number"),
-        to: evento.getHeader("Caller-Destination-Number")
-      };
+    const chamada = {
+      callid: evento.getHeader("Channel-Call-UUID"),
+      from: evento.getHeader("Caller-Caller-ID-Number"),
+      to: evento.getHeader("Caller-Destination-Number")
+    };
 
-      removerChamadaDaLista(chamada.callid);
-    }
+    removerChamadaDaLista(chamada.callid);
   });
 });
 
