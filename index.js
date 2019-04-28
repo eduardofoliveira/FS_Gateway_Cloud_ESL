3899b64d4ccb8f6480802a8e0d2082f92f628cab
eduardofoliveira <eduardo_felipe_oliveira@yahoo.com.br>
2019-04-28 16:05:35 -0300

    Desenvolvendo

--- index.js
+++ index.js
@@ -1,43 +1,22 @@
-FS = require("esl");
+const esl = require("modesl");
 
-const client = FS.client({ all_events: false }, function() {
-  this.onceAsync("CHANNEL_ANSWER", call => {
-    console.log("Call was answered \r\n");
-    console.log(call);
-  });
+conn = new esl.Connection("127.0.0.1", 8021, "ClueCon", function() {
+  conn.events("json", "all");
 
-  /*this.onceAsync("CHANNEL_HANGUP").then(function(call) {
-    console.log("Call hangup \r\n");
-    console.log(call);
-  });
-
-  this.onceAsync("CHANNEL_CREATE").then(function(call) {
-    console.log("Channel Create \r\n");
-
-    console.log(call.body["Call-Direction"]);
-
-    //if (call.body["Call-Direction"] === "inbound") {
-    //  console.log(call);
-    //}
-  });
-
-  this.onceAsync("CHANNEL_HANGUP_COMPLETE").then(function(call) {
-    console.log("Call was disconnected \r\n");
-    console.log(call);
-  });
-
-  this.on("SOME_MESSAGE", function(call) {
-    console.log("Got Some Message");
-    console.log(call);
-  });*/
-
-  this.event_json(
-    "CHANNEL_ANSWER",
+  conn.subscribe([
     "CHANNEL_CREATE",
-    "CHANNEL_HANGUP",
-    "CHANNEL_HANGUP_COMPLETE",
-    "SOME_MESSAGE"
-  );
-});
+    "CHANNEL_CALLSTATE",
+    "CHANNEL_STATE",
+    "CHANNEL_EXECUTE",
+    "CHANNEL_EXECUTE_COMPLETE",
+    "CHANNEL_DESTROY"
+  ]);
 
-client.connect(8021, "127.0.0.1");
+  conn.on("esl::event::CHANNEL_CREATE::*", function(call) {
+    console.log(call);
+  });
+
+  conn.on("esl::event::CHANNEL_HANGUP_COMPLETE::*", function(call) {
+    console.log(call);
+  });
+});
