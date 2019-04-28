e9a5b96ee74ed144527952ed2955256b09fb0ee4
eduardofoliveira <eduardo_felipe_oliveira@yahoo.com.br>
2019-04-28 15:04:12 -0300

    Desenvolvendo

--- index.js
+++ index.js
@@ -2,30 +2,36 @@ FS = require("esl");
 
 const client = FS.client(function() {
   this.onceAsync("CHANNEL_ANSWER").then(function(call) {
-    console.log("Call was answered");
+    console.log("Call was answered \r\n");
     console.log(call);
   });
 
   this.onceAsync("CHANNEL_HANGUP").then(function(call) {
-    console.log("Call hangup");
+    //console.log("Call hangup");
+    //console.log(call);
+  });
+
+  this.onceAsync("CHANNEL_CREATE").then(function(call) {
+    console.log("Channel Create \r\n");
     console.log(call);
   });
 
   this.onceAsync("CHANNEL_HANGUP_COMPLETE").then(function(call) {
-    console.log("Call was disconnected");
+    console.log("Call was disconnected \r\n");
     console.log(call);
   });
 
   this.on("SOME_MESSAGE", function(call) {
-    console.log("Got Some Message");
-    console.log(call);
+    //console.log("Got Some Message");
+    //console.log(call);
   });
 
   this.event_json(
     "CHANNEL_ANSWER",
-    "CHANNEL_HANGUP",
-    "CHANNEL_HANGUP_COMPLETE",
-    "SOME_MESSAGE"
+    "CHANNEL_CREATE",
+    //"CHANNEL_HANGUP",
+    "CHANNEL_HANGUP_COMPLETE"
+    //"SOME_MESSAGE"
   );
 });
 
