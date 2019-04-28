3d5b5b5e5c2564b6fbce68b945bc07e07358622f
eduardofoliveira <eduardo_felipe_oliveira@yahoo.com.br>
2019-04-28 17:50:19 -0300

    Add Server Http

--- index.js
+++ index.js
@@ -6,93 +6,107 @@ const api = axios.create({
   baseURL: "http://contact.cloudcom.com.br"
 });
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("");
+waitTime = 20000;
 
-  //chamada/11999683333/551137115000/Eduardo/cloud.cloudcom.com.br/gfjdghkd55dkjhfd/RINGING
+let doConnect = () => {
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("");
 
-  const padrao = /\/(.*)\/(.*)\/(.*)\/(.*)\/(.*)\/(.*)\/(.*)/;
-  const retorno = padrao.exec(req.url);
-  if (retorno) {
-    let [, , from, to, username, domain, call_id, method] = retorno;
+    //chamada/11999683333/551137115000/Eduardo/cloud.cloudcom.com.br/gfjdghkd55dkjhfd/RINGING
 
-    realTo = getDestinationCall(from);
-    if (realTo) {
-      to = realTo;
+    const padrao = /\/(.*)\/(.*)\/(.*)\/(.*)\/(.*)\/(.*)\/(.*)/;
+    const retorno = padrao.exec(req.url);
+    if (retorno) {
+      let [, , from, to, username, domain, call_id, method] = retorno;
+
+      realTo = getDestinationCall(from);
+      if (realTo) {
+        to = realTo;
+      }
+
+      api.get(
+        `/chamada/${from}/${to}/${username}/${domain}/${call_id}/${method}`
+      );
     }
+  });
 
-    api.get(
-      `/chamada/${from}/${to}/${username}/${domain}/${call_id}/${method}`
-    );
-  }
-});
+  server.listen(80);
 
-server.listen(80);
+  let chamadas_ativas = [];
 
-let chamadas_ativas = [];
-
-const removerChamadaDaLista = id => {
-  for (let index = 0; index < chamadas_ativas.length; index++) {
-    if (chamadas_ativas[index].callid === id) {
-      chamadas_ativas.splice(index, 1);
+  const removerChamadaDaLista = id => {
+    for (let index = 0; index < chamadas_ativas.length; index++) {
+      if (chamadas_ativas[index].callid === id) {
+        chamadas_ativas.splice(index, 1);
+      }
     }
-  }
-  console.log(`Chamada com ID: ${id} removida`);
-};
+  };
 
-const contemNaLista = id => {
-  for (let index = 0; index < chamadas_ativas.length; index++) {
-    if (chamadas_ativas[index].callid === id) {
-      console.log(`Chamada com ID: ${id} já esta na lista`);
-      return true;
+  const contemNaLista = id => {
+    for (let index = 0; index < chamadas_ativas.length; index++) {
+      if (chamadas_ativas[index].callid === id) {
+        return true;
+      }
     }
-  }
-};
+  };
 
-const getDestinationCall = from => {
-  for (let index = 0; index < chamadas_ativas.length; index++) {
-    if (chamadas_ativas[index].from === from) {
-      return chamadas_ativas[index].to;
+  const getDestinationCall = from => {
+    for (let index = 0; index < chamadas_ativas.length; index++) {
+      if (chamadas_ativas[index].from === from) {
+        return chamadas_ativas[index].to;
+      }
     }
-  }
-};
+  };
 
-conn = new esl.Connection("127.0.0.1", 8021, "ClueCon", function() {
-  conn.events("json", "all");
+  conn = new esl.Connection("127.0.0.1", 8021, "ClueCon", function() {
+    conn.events("json", "all");
 
-  conn.subscribe(["CHANNEL_CREATE", "CHANNEL_HANGUP_COMPLETE"]);
+    conn.subscribe(["CHANNEL_CREATE", "CHANNEL_HANGUP_COMPLETE", "disconnect"]);
 
-  conn.on("esl::event::CHANNEL_CREATE::*", evento => {
-    if (evento.getHeader("Call-Direction") === "outbound") {
+    conn.on("esl::event::CHANNEL_CREATE::*", evento => {
+      if (evento.getHeader("Call-Direction") === "outbound") {
+        const chamada = {
+          callid: evento.getHeader("Channel-Call-UUID"),
+          from: evento.getHeader("Caller-Caller-ID-Number"),
+          to: evento.getHeader("Caller-Destination-Number")
+        };
+
+        if (!contemNaLista(chamada.callid)) {
+          chamadas_ativas.push(chamada);
+        }
+      }
+    });
+
+    conn.on("esl::event::CHANNEL_HANGUP_COMPLETE::*", evento => {
       const chamada = {
         callid: evento.getHeader("Channel-Call-UUID"),
         from: evento.getHeader("Caller-Caller-ID-Number"),
         to: evento.getHeader("Caller-Destination-Number")
       };
 
-      if (!contemNaLista(chamada.callid)) {
-        chamadas_ativas.push(chamada);
+      removerChamadaDaLista(chamada.callid);
+    });
 
-        console.log("Chamada Adicionada");
-        console.log(chamada);
-        console.log("");
-      }
-    }
+    conn.on("error", error => {
+      console.log("caiu");
+      let data = new Date();
+      console.log(`${error.code} - ${data.toLocaleString()}`);
+      setTimeout(doConnect, waitTime);
+    });
+
+    conn.on("esl::event::disconnect::notice", () => {
+      let data = new Date();
+      console.log(`desconectou - ${data.toLocaleString()}`);
+      setTimeout(doConnect, waitTime);
+    });
   });
+};
 
-  conn.on("esl::event::CHANNEL_HANGUP_COMPLETE::*", evento => {
-    const chamada = {
-      callid: evento.getHeader("Channel-Call-UUID"),
-      from: evento.getHeader("Caller-Caller-ID-Number"),
-      to: evento.getHeader("Caller-Destination-Number")
-    };
+doConnect();
 
-    removerChamadaDaLista(chamada.callid);
-  });
+process.on("uncaughtException", function(err) {
+  let data = new Date();
+  console.log(`${err.code} - ${data.toLocaleString()}`);
+  setTimeout(doConnect, waitTime);
 });
-
-setInterval(() => {
-  console.log("Chamadas Ativas");
-  console.log(chamadas_ativas);
-}, 10000);
