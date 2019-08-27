f2ee6582372c35478423ae7104f576ec7248a5a4
eduardofoliveira <eduardo_felipe_oliveira@yahoo.com.br>
2019-08-27 11:49:21 -0300

    Add on ESTABLISHED

--- app.js
+++ app.js
@@ -1,73 +1,73 @@
-const express = require("express");
-const axios = require("axios");
-const app = express();
+const express = require('express')
+const axios = require('axios')
+const app = express()
 
 const api = axios.create({
-  baseURL: "http://35.171.122.245:83"
-});
+  baseURL: 'http://35.171.122.245:83'
+})
 
-let navegacaoUra = [];
+let navegacaoUra = []
 
 app.get(`/identificacao/:from/:to/:username/:domain/:call_id/:method`, (req, res) => {
-  res.send();
-  let { from, to, username, domain, call_id, method } = req.params;
+  res.send()
+  let { from, to, username, domain, call_id, method } = req.params
 
-  if (method === "RINGING") {
+  if (method === 'RINGING' || method === 'ESTABLISHED') {
     let existe = navegacaoUra.filter(item => {
-      return item.from === from && item.to === to;
-    });
+      return item.from === from && item.to === to
+    })
 
-    console.log(existe);
+    console.log(existe)
 
     if (existe.length > 0) {
-      api.get(`/chamada/${from}/${to}/${username}/${domain}/${call_id}/${method}/${existe[0].opcao.join(".")}`);
+      api.get(`/chamada/${from}/${to}/${username}/${domain}/${call_id}/${method}/${existe[0].opcao.join('.')}`)
     } else {
-      api.get(`/chamada/${from}/${to}/${username}/${domain}/${call_id}/${method}`);
+      api.get(`/chamada/${from}/${to}/${username}/${domain}/${call_id}/${method}`)
     }
   }
 
-  if (method === "DISCONNECTED") {
+  if (method === 'DISCONNECTED') {
     navegacaoUra = navegacaoUra.filter(item => {
-      return item.from !== from && item.to !== to;
-    });
+      return item.from !== from && item.to !== to
+    })
 
-    api.get(`/chamada/${from}/${to}/${username}/${domain}/${call_id}/${method}`);
+    api.get(`/chamada/${from}/${to}/${username}/${domain}/${call_id}/${method}`)
   }
 
-  if (method === "HELD") {
-    api.get(`/chamada/${from}/${to}/${username}/${domain}/${call_id}/DISCONNECTED`);
+  if (method === 'HELD') {
+    api.get(`/chamada/${from}/${to}/${username}/${domain}/${call_id}/DISCONNECTED`)
   }
-});
+})
 
 app.get(`/ura/:from/:to/:opcao/:domain`, (req, res) => {
-  res.send();
-  let { from, to, opcao, domain } = req.params;
+  res.send()
+  let { from, to, opcao, domain } = req.params
 
   let existe = navegacaoUra.filter(item => {
-    return item.from === from && item.to === to;
-  });
+    return item.from === from && item.to === to
+  })
 
   if (existe.length > 0) {
     navegacaoUra = navegacaoUra.map(item => {
       if (item.from === from && item.to === to) {
-        item.opcao = [...item.opcao, opcao];
+        item.opcao = [...item.opcao, opcao]
       }
-      return item;
-    });
+      return item
+    })
   } else {
     navegacaoUra.push({
       from,
       to,
       opcao: [opcao],
       domain
-    });
+    })
   }
-});
+})
 
-app.get("/status", (req, res) => {
-  res.status(200).json({ navegacaoUra });
-});
+app.get('/status', (req, res) => {
+  res.status(200).json({ navegacaoUra })
+})
 
 app.listen(81, () => {
-  console.log("Running at port 81");
-});
+  console.log('Running at port 81')
+})
