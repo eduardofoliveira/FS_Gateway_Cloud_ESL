cefd38e999d3903c90a4c705ee4282d9a9538fa0
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2020-03-13 18:50:21 -0300

    stringify

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const connector = require('./fs-connector')
 
 connector.on('create', chamada => {
-  console.log('create: ' + chamada)
+  console.log('create: ' + JSON.stringify(chamada, null, 2))
 })
 
 connector.on('hangup', chamada => {
-  console.log('hangup: ' + chamada)
+  console.log('hangup: ' + JSON.stringify(chamada, null, 2))
 })
