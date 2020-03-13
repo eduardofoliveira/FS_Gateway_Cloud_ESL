cdd01d3fa3059dd9df904929e6f2b56c6420cc92
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2020-03-13 18:54:37 -0300

    Organizar em lista

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const connector = require('./fs-connector')
 
+let lista = {}
+
 connector.on('create', chamada => {
-  console.log('create: ' + JSON.stringify(chamada, null, 2))
+  // console.log('create: ' + JSON.stringify(chamada, null, 2))
+  lista[chamada.callid] = {
+    from: chamada.from,
+    to: chamada.to
+  }
 })
 
 connector.on('hangup', chamada => {
-  console.log('hangup: ' + JSON.stringify(chamada, null, 2))
+  // console.log('hangup: ' + JSON.stringify(chamada, null, 2))
+  delete lista[chamada.callid]
 })
+
+setInterval(() => {
+  console.log(lista)
+  console.log('')
+}, 5000)
